3617bee4d83ef45b8993c114f26e4dbf77356006
Valentine B <vanta11a@yandex.ru>
2025-01-07 13:14:10 +0500

    Первая реализация по ТЗ 10 спринта

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,23 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.Duration;
+import java.time.Instant;
+
+@Data
 public class Film {
+    Long id;
+
+    @NotNull(message = "Не передано название")
+    @NotBlank(message = "Название не должно быть пустым")
+    String name;
+
+    String description;
+
+    Instant releaseDate;
+
+    Duration duration;
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.Instant;
+
+@Data
+@EqualsAndHashCode(of = {"login"})
+public class User {
+    Long id;
+
+    @NotNull(message = "Не передан email")
+    @NotBlank(message = "Email не должен быть пустым")
+    @Email(message = "Неправильный формат email")
+    String email;
+
+    @NotNull(message = "Не передан логин")
+    @NotBlank(message = "Логин не должен быть пустым")
+    String login;
+
+    String name;
+
+    @PastOrPresent(message = "Дата рождения не может быть в будущем")
+    Instant birthday;
+}
